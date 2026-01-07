abc7d2a2cfcac13e6f719d08469dc35c52be9943
prakashornold <prakashornold@gmail.com>
2026-01-07 19:15:37 +0530

    Updated 20260107185253_add_description_to_java_problems.sql

--- supabase/migrations/20260107185253_add_description_to_java_problems.sql
+++ supabase/migrations/20260107185253_add_description_to_java_problems.sql
@@ -13,12 +13,4 @@
 */
 
 -- Add description column to java_problems table
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_name = 'java_problems' AND column_name = 'description'
-  ) THEN
-    ALTER TABLE java_problems ADD COLUMN description text NOT NULL DEFAULT '';
-  END IF;
-END $$;
+ALTER TABLE java_problems ADD COLUMN description text NOT NULL DEFAULT '';
